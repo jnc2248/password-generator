ee8b0d95659effb250ab6938e1ce9851c4b6204b
[j chun] <[justinchun224@gmail.com]>
2023-09-10 18:09:58 -0700

    fixed if/else statements in js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,11 @@
-// Assignment code here
 var lower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var upper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var numeric = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
 var specialChar = ["!", "(", ")", "-", ".", "?", "[", "]", "_", "~", ";", ":", "@", "#", "$", "%", "^", "&", "*", "=", "+"];
 var combined = [];
-
-
-// Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
-var generatePassword = function () {
+var buttonPress = function () {
   var lengthSelect = window.prompt("Please enter desired length for password. Must be between 8-128.")
   if (!lengthSelect) {
     return;
@@ -26,25 +22,47 @@ var generatePassword = function () {
 
   if (upperSelect && numSelect && specialSelect) {
     combined = combined.concat(lower, upper, numeric, specialChar);
+  } else if (upperSelect && numSelect && !specialSelect) {
+    combined = combined.concat(lower, upper, numeric);
+  } else if (upperSelect && !numSelect && specialSelect) {
+    combined = combined.concat(lower, upper, specialChar);
+  } else if (!upperSelect && numSelect && specialSelect) {
+    combined = combined.concat(lower, numeric, specialChar);
+  } else if (upperSelect && !numSelect && !specialSelect) {
+    combined = combined.concat(lower, upper);
+  } else if (!upperSelect && !numSelect && specialSelect) {
+    combined = combined.concat(lower, specialChar);
+  } else if (!upperSelect && numSelect && !specialSelect) {
+    combined = combined.concat(lower, numeric);
+  } else {
+    combined = combined.concat(lower);
   };
 
+  console.log(combined);
+
+  function genPassword(lengthSelect) {
+    var initString = combined.toString();
+    var finalString = initString.replace(",","");
+    var characters = finalString;
+    var result = '';
+    var charactersLength = characters.length;
+    var counter = 0;
+    while (counter < lengthSelect) {
+      result += characters.charAt(Math.floor(Math.random() * charactersLength));
+      counter += 1;
+    }
+    
+    console.log(result);
+  };
+
+  console.log(genPassword(lengthSelect));
+
 };
 
+var password = buttonPress;
+var passwordText = document.querySelector("#password");
 
+passwordText.value = password;
 
-// How to return error msg if not between 8-128?
-
-
-
-// Write password to the #password input
-var buttonPress = function() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-};
-
-// Add event listener to generate button
 generateBtn.addEventListener("click", buttonPress);
 
